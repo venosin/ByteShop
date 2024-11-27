d193e81fac4cbc9a3359bf83eaad8803fc5e6ee8
Wilfredo <xvwilito11@gmail.com>
2024-11-27 11:34:15 -0600

    comenzando crud de categorias en react web

---
+++ frontend-byteshop/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Categories from './screens/Categories';
+
+function App() {
+  return (
+<>
+    <Categories/>
+</>
+  );
+}
+
+export default App;
