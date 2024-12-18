63db994a30c9370ae73bafbe9c5561d5acf5ecb8
exequiel-miranda <exequiel.miranda245@gmail.com>
2024-12-18 08:09:15 -0600

    add validateTokem middleware for protected endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,8 @@ import purchasesRoutes from './routes/purchases.js'
 import loginRoutes from './routes/login.js'
 import registerRoutes from './routes/register.js'
 import logoutRoutes from './routes/logout.js'
+import {authRequire} from './middlewares/validateToken.js'
+import cookieParser from 'cookie-parser'
 
 
 const app = express();
@@ -27,21 +29,10 @@ const app = express();
 // TODO: aqui van algunos ajustes
 
 // Middlewares
-/*
-Un middleware es una función que se ejecuta entre la solicitud (request) del cliente y la respuesta (response) del servidor. 
-Puede realizar tareas como validar datos, registrar actividad o transformar la solicitud antes de pasarla a la siguiente función.
-Por ejemplo,
-Piensa en un guardia de seguridad en la entrada de un edificio. Antes de que alguien entre, el guardia revisa su identificación
-(validación) o le da una tarjeta de acceso (modificación). Este proceso sucede antes de que la persona pueda continuar hacia su destino.
-*/
 app.use(cors());
-/*
-PD: Cors: es un mecanismo que permite que un servidor acepte solicitudes desde dominios diferentes al suyo. 
-Por defecto, los navegadores bloquean estas solicitudes para proteger datos sensibles.
-Esto permite que aplicaciones externas puedan hacer solicitudes al servidor. Por ejemplo, 
-si el servidor está en http://api.ejemplo.com, aplicaciones desde http://frontend-app.com pueden enviar solicitudes sin ser bloqueadas.
- */
 app.use(express.json());
+app.use(cookieParser());
+
 
 // Routes
 //El sistema web puede acceder a las siguientes rutas:
@@ -57,6 +48,7 @@ app.use('/purchases', purchasesRoutes)
 app.use('/login', loginRoutes)
 app.use('/register', registerRoutes)
 app.use('/logout', logoutRoutes)
+//PD: se agrega authRequire antes de la ruta para que el endpoint necesite un token
 
 
 export default app;
---
+++ backend/src/middlewares/validateToken.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+export const authRequire = (req, res, next) => {
+  try {
+    // Validar si existen cookies
+    if (!req.cookies) {
+      return res.status(401).json({ message: "No cookies found, authorization required" });
+    }
+
+    // Extraer el token de las cookies
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).json({ message: "Token not provided, unauthorized" });
+    }
+
+    // Verificar el token
+    const decoded = jwt.verify(token, "secret123");
+    req.user = decoded;
+
+    next(); // Continuar al siguiente middleware o controlador
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(403).json({ message: "Token expired, please log in again" });
+    }
+    if (err.name === "JsonWebTokenError") {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    return res.status(500).json({ message: "Internal server error", error: err.message });
+  }
+};
